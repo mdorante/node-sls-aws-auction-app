e21ce25b14724b57ecd93658c41643cc4111342c
mdorante <mdorante10@gmail.com>
2020-08-24 22:15:41 +0000

    re-format random id generator

--- src/lib/s3RandomId.js
+++ src/lib/s3RandomId.js
@@ -3,5 +3,4 @@ const randomNum = () => Math.floor(Math.random() * 100);
 const uuid = () =>
   `${randomNum()}${randomNum()}-${randomNum()}${randomNum()}-${randomNum()}${randomNum()}`;
 
-//export const s3Id = uuid;
 module.exports.s3Id = uuid;
